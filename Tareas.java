2680c5f180aa650103e42fa624f9319e72b8c7d6
PaulaBenlloch <paubengon@alu.edu.gva.es>
2024-12-10 14:47:46 +0100

    Creado archivo Tareas.java que realiza la suma de dos números introducidos por teclado

---
+++ Tareas.java
@@ -0,0 +1,9 @@
+public class Tareas {
+    public static void main (String[] args){
+        int a = 5;
+        int b = 7;
+        int suma;
+        suma = a+b;
+        System.out.print("La suma de " + a + " + " + b + " es " + suma);
+    }
+}

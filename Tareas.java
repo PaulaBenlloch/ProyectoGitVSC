7d74e19761a2c9886f016afeea49e5ea0c5195d3
PaulaBenlloch <paubengon@alu.edu.gva.es>
2024-12-10 14:53:18 +0100

    Creado archivo Tareas.java el cuál realiza la suma de dos números

--- Tareas.java
+++ Tareas.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
 public class Tareas {
     public static void main (String[] args){
-        int a = 5;
-        int b = 7;
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Introduce el primer número: ");
+        int a = sc.nextInt();
+        System.out.print("Introduce el segundo número: ");
+        int b = sc.nextInt();
         int suma;
         suma = a+b;
-        System.out.print("La suma de " + a + " + " + b + " es " + suma);
+        System.out.print("La suma de " + a + " + " + b + " es: " + suma);
     }
 }
